3b20825782fae40738e2246e6474e9430a37aa9e
GiovannaSoria <gmsoria@espe.edu.ec>
2025-06-21 16:53:29 -0500

    proyecto antiguo

---
+++ src/main/java/com/originacion/contratos/pagares/repository/PagareRepository.java
@@ -0,0 +1,24 @@
+package com.originacion.contratos.pagares.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+import com.originacion.contratos.pagares.model.Pagare;
+
+public interface PagareRepository extends JpaRepository<Pagare, Long> {
+
+    List<Pagare> findByIdSolicitud(Long idSolicitud);
+    
+    List<Pagare> findByIdSolicitudOrderByNumeroCuota(Long idSolicitud);
+    
+    Optional<Pagare> findByIdSolicitudAndNumeroCuota(Long idSolicitud, Integer numeroCuota);
+    
+    List<Pagare> findByNumeroCuota(Integer numeroCuota);
+    
+    boolean existsByIdSolicitud(Long idSolicitud);
+    
+    Long countByIdSolicitud(Long idSolicitud);
+    
+    void deleteByIdSolicitud(Long idSolicitud);
+} 
